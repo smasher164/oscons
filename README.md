bd245d76796a5c9ae3588e65b03116d9be53c65e
smasher164 <aindurti@gmail.com>
2019-10-01 04:37:31 -0400

    Add Boot HelloWorld example for BIOS

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # OS Construction
 
-This repository serves as a companion to my blog series, "Notes on OS Construction."
-All code is on separate branches and will also be linked below for easy access, as I add them.
-Additionally, all code is released to the public domain (see the LICENSE.md file).
+This branch contains a BIOS-based bootloader for x86 that prints "Hello, World!" to the screen. YASM is used to assemble the source, and is available at https://yasm.tortall.net. QEMU is used to emulate the hardware and BIOS necessary to run the bootloader, and is available at https://www.qemu.org/.
+
+QEMU can be used to execute the flat binary file by treating it as a disk. Either qemu-system-i386 or qemu-system-x86_64 can be used to run the disk image. The following commands can be used to build and run the bootloader in QEMU:
+
+```
+$ yasm -f bin boot.s -o boot.img
+$ qemu-system-<i386|x86_64> -hda boot.img
+```
+
+Which should result in output like the following:
+![screenshot](https://user-images.githubusercontent.com/12636891/65946766-d705e280-e404-11e9-92d2-0abeb4f59641.png)
---
+++ boot.s
@@ -0,0 +1,26 @@
+;
+; Simple BIOS-based bootloader program for x86 that prints "Hello, World!" to
+; the screen. This file should be assembled with YASM, available at
+; https://yasm.tortall.net.
+
+    bits 16                         ; Tell YASM these are 16-bit instructions
+    org 0x7C00                      ; Tell YASM to load bootloader at 0x7C00
+    mov si, HELLO_STRING            ; si = HELLO_STRING
+    mov ah, 0x0E                    ; set interrupt handler to TTY printer
+
+; Loop over and print each character in the string until NULL terminator
+; is encountered.
+
+.loop:
+    lodsb                           ; al = *HELLO_STRING; HELLO_STRING++.
+    cmp al, 0
+    jz end                          ; if (al == 0) goto end
+    int 0x10                        ; execute the interrupt for video services
+    jmp .loop
+
+end:
+    hlt ; exit bootloader
+
+HELLO_STRING: db "Hello, World!", 0 ; Declare NULL-terminated string constant
+    times 510 - ($-$$) db 0         ; Zero out the remaining of the 512 bytes
+    dw 0xAA55                       ; Set last two bytes to signature
